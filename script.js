a8fa729dd0dd21d5d30e913979e918c3071650c4
Johanna-hub <jyherman@gmail.com>
2019-02-05 09:35:58 +0000

    Remove commented out code

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-//         function show(e) {
-//   const { type, angle } = screen.orientation;
-//   console.log(`Orientation type is ${type} & angle is ${angle}.`);
-//   console.log(`event is ${event.type}`);
-// }
-// screen.orientation.addEventListener("change", (e) => {
-//     show(e);
-// })
-
 const eventChange = new Promise(function(resolve, reject) {
   screen.orientation.addEventListener("change", e => {
     resolve(e.type);
   });
-});
-
-const lockCheck = new Promise(function(resolve, reject) {
-  screen.orientation.lock("landscape");
-  resolve(screen.orientation.type);
 });
 
 function responseCheck(response) {
@@ -34,6 +20,3 @@ function orderCheck() {
     .catch(error => console.log(`error is ${error}`));
 }
 
-// function eventCheck() {
-//   eventChange.then(response => console.log(response));
-// }

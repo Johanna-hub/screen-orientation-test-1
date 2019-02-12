aa3d8c739bc553fdb6e2efe1a5a86c352218adab
Johanna-hub <jyherman@gmail.com>
2019-02-12 20:16:25 +0000

    basic lock tests

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const promisesToReject = invalid_lock_types.map(type =>
 );
 
 async function invalidLockCheck() {
-  // document.documentElement.requestFullscreen();
+  document.documentElement.requestFullscreen();
   try {
     await Promise.all(promisesToReject);
   } catch (err) {
@@ -51,10 +51,83 @@ async function invalidLockCheck() {
 }
 
 async function emptyLockCheck() {
-  // document.documentElement.requestFullscreen();
+  document.documentElement.requestFullscreen();
   try {
     await screen.orientation.lock();
   } catch (err) {
     console.log(`error: ${err}`);
   }
 }
+
+// Testing basic lock functions
+
+// "Test that screen.orientation.unlock() doesn't throw when there is no lock");
+function unlock() {
+  screen.orientation.unlock();
+}
+
+// "Test that screen.orientation.unlock() returns a void value"
+
+function voidLock() {
+  const value = screen.orientation.unlock();
+  console.log(value, "should be undefined");
+}
+
+// "Test that screen.orientation.lock returns a promise which will be fulfilled with a void value."
+async function lockPromise() {
+  await document.documentElement.requestFullscreen();
+  const value = await screen.orientation.lock("any");
+  console.log(value, "should be undefined");
+}
+
+// "Test that screen.orientation.lock returns a pending promise
+async function lockTest() {
+  const orientations = [
+    "any",
+    "natural",
+    "portrait",
+    "landscape",
+    "portrait-secondary",
+    "landscape-primary",
+    "landscape-secondary",
+    "portrait-primary"
+  ];
+  await document.documentElement.requestFullscreen();
+  for (const orientation of orientations) {
+    const promiseToChange = screen.orientation.lock(orientation);
+    console.log(promiseToChange instanceof Promise);
+    await promiseToChange;
+    const type = screen.orientation.type;
+    switch (orientation) {
+      case "any":
+        break;
+      case "natural":
+        console.log(type, "should be portrait-primary or landscape-primary");
+        break;
+      case "portrait":
+        console.log(type, "should be portrait-primary or portrait-secondary");
+        break;
+      case "landscape":
+        console.log(type, "should be landscape-primary or landscape-secondary");
+        break;
+      default:
+        console.log(type, orientation, "Expected orientation to change");
+        break;
+    }
+  }
+  screen.orientation.unlock();
+}
+
+promise_test(async t => {
+  const preType = screen.orientation.type;
+  const isPortrait = preType.includes("portrait");
+  const newType = `${isPortrait ? "landscape" : "portrait"}-primary`;
+  const p = screen.orientation.lock(newType);
+  assert_equals(
+    screen.orientation.type,
+    preType,
+    "Must not change orientation until next spin of event loop"
+  );
+  await p;
+  assert_equals(screen.orientation.type, newType);
+}, "Test that screen.orientation.lock() is actually async");

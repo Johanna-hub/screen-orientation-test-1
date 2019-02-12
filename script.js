c8b1db1131ce51d1ab34fbd1cd2f0cf875d69c77
Johanna-hub <jyherman@gmail.com>
2019-02-12 18:06:31 +0000

    Invalid lock arguments test

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Testing event fires before promise
+
 const promiseToChange = new Promise(resolve => {
   screen.orientation.addEventListener("change", resolve);
 });
@@ -23,3 +25,36 @@ async function orderCheck() {
   }
 }
 
+// Testing lock throws error if non-valid input or empty input given
+const invalid_lock_types = [
+  "invalid-orientation",
+  null,
+  undefined,
+  123,
+  window,
+  "",
+  true,
+  ["portrait-primary", "landscape-primary"]
+];
+
+const promisesToReject = invalid_lock_types.map(type =>
+  screen.orientation.lock(type)
+);
+
+async function invalidLockCheck() {
+  document.documentElement.requestFullscreen();
+  try {
+    await Promise.all(promisesToReject);
+  } catch (err) {
+    console.log(`error: ${err}`);
+  }
+}
+
+async function emptyLockCheck() {
+  document.documentElement.requestFullscreen();
+  try {
+    await screen.orientation.lock();
+  } catch (err) {
+    console.log(`error: ${err}`);
+  }
+}

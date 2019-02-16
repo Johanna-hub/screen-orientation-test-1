6fff2e1bdcb59d19ca6f31006783dae4d18f16a2
Johanna-hub <jyherman@gmail.com>
2019-02-16 09:43:44 +0000

    testing current angle

--- script.js
+++ script.js
@@ -204,22 +204,20 @@ async function notSupported() {
 // Check the angle values
 
 async function angleTest() {
-  const type = screen.orientation.type;
-  const angle = screen.orientation.angle;
   await document.documentElement.requestFullscreen();
   await screen.orientation.lock("portrait-primary");
-  console.log(`${angle} should be 0, 90 or 270`);
+  console.log(`${screen.orientation.angle} should be 0, 90 or 270`);
   await screen.orientation.lock("portrait-secondary");
-  console.log(`${angle} should be 180, 90 or 270`);
+  console.log(`${screen.orientation.angle} should be 180, 90 or 270`);
   await screen.orientation.lock("landscape-primary");
-  console.log(`${angle} should be 0, 90 or 270`);
+  console.log(`${screen.orientation.angle} should be 0, 90 or 270`);
   await screen.orientation.lock("landscape-secondary");
-  console.log(`${angle} should be 180, 90 or 270`);
+  console.log(`${screen.orientation.angle} should be 180, 90 or 270`);
   screen.orientation.unlock();
   if (screen.width > screen.height) {
-    console.log(`${type} should be landscape-primary or landscape-secondary`);
+    console.log(`${screen.orientation.type} should be landscape-primary or landscape-secondary`);
   } else if (screen.width < screen.height) {
-    console.log(`${type} should be portrait-primary or portrait-secondary`);
+    console.log(`${screen.orientation.type} should be portrait-primary or portrait-secondary`);
   }
   return document.exitFullscreen();
 }

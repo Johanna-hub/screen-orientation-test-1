b120e0e245e308d9bc692b3571a7357c7e019114
Johanna-hub <jyherman@gmail.com>
2019-03-12 13:20:25 +0000

    popup test 4

--- script.js
+++ script.js
@@ -436,9 +436,9 @@ async function unlockOnChange() {
 
 async function popupWindow() {
   const popWindow = window.open("./resources/blank.html", "test");
-  popWindow.onload = 
-  function navigation() {
-    popWindow.location.href = "https://github.com";
-  }
+  // popWindow.onload = 
+  // function navigation() {
+  //   popWindow.location.href = "https://github.com";
+  // }
   await screen.orientation.lock("landscape-primary");
 }
